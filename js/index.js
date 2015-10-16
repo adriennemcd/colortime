99808d1a00513d0ba7028a6f503c15ddfa97f052
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2015-10-16 13:49:26 -0400

    set up enviro, added page of color divs, working on color hovering

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var html = '';
+var rgbColor;
+
+function randomRGB() {
+  return Math.floor(Math.random() * 256 );
+}
+
+function randomColor() {
+  var color = 'rgb('
+  color += randomRGB() + ', ';
+  color += randomRGB() + ', ';
+  color += randomRGB() + ')';
+  return color;
+}
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function hoverColor() {
+    $('.all').mouseover(function(){
+        console.log(this.id);
+        $('div#color' + this.id).css("background-color", "yellow");
+        //console.log(test);
+    });
+}
+
+for ( var i = 0; i < 100; i += 1) {
+  rgbColor = randomColor();
+  html += '<div class="all" id="color' + i + '" style="background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
+hoverColor();
