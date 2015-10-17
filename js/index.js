ade0faa4b125818652d28ac3dc323aa3564a1d87
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2015-10-17 10:54:11 -0400

    info box added with hover/click color

--- js/index.js
+++ js/index.js
@@ -2,32 +2,45 @@ var html = '';
 var rgbColor;
 
 function randomRGB() {
-  return Math.floor(Math.random() * 256 );
+    return Math.floor(Math.random() * 256 );
 }
 
 function randomColor() {
-  var color = 'rgb('
-  color += randomRGB() + ', ';
-  color += randomRGB() + ', ';
-  color += randomRGB() + ')';
-  return color;
+    var color = 'rgb('
+    color += randomRGB() + ', ';
+    color += randomRGB() + ', ';
+    color += randomRGB() + ')';
+    return color;
 }
 
 function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = message;
+}
+
+function print2(message) {
+    var infoDiv = document.getElementById('colorClick');
+    infoDiv.innerHTML = message;
 }
 
 function hoverColor() {
     $('.all').mouseover(function(){
-        console.log(this.id);
-        $('#' + this.id).css("background-color", "yellow");
+        $('#' + this.id).css("border", "3px solid white");
+    });
+    $('.all').mouseout(function(){
+        $('#' + this.id).css("border", "none");
+    });
+    $('.all').click(function(){
+        var bckgrnd = $( this ).css( 'background-color' );
+        var colorInfo = '<p>' + $( this ).css( 'background-color' ) + '</p>';
+        $('#tiny').css( 'background-color', bckgrnd );
+        print2(colorInfo);
     });
 }
 
 for ( var i = 0; i < 100; i += 1) {
-  rgbColor = randomColor();
-  html += '<div class="all" id="color' + i + '" style="background-color:' + rgbColor + '"></div>';
+    rgbColor = randomColor();
+    html += '<div class="all" id="color' + i + '" style="background-color:' + rgbColor + '"></div>';
 }
 
 print(html);
