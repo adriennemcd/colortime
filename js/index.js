ef9fa76d334631c7dc614507686d9a953070a0ae
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2015-10-19 18:53:08 -0400

    able to select multiple colors

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var html = '';
 var rgbColor;
+var selectedColors = [];
+var selectedColorBlocks = [];
 
 function randomRGB() {
     return Math.floor(Math.random() * 256 );
@@ -13,6 +15,11 @@ function randomColor() {
     return color;
 }
 
+for ( var i = 0; i < 100; i += 1) {
+    rgbColor = randomColor();
+    html += '<div class="all" id="color' + i + '" style="background-color:' + rgbColor + '"></div>';
+}
+
 function print(message) {
     var outputDiv = document.getElementById('output');
     outputDiv.innerHTML = message;
@@ -23,7 +30,17 @@ function print2(message) {
     infoDiv.innerHTML = message;
 }
 
-function hoverColor() {
+function print3(message) {
+    var infoDiv = document.getElementById('tinySquares');
+    infoDiv.innerHTML = message;
+}
+
+function Color(bckgrnd, identity) {
+    this.bckgrnd = bckgrnd;
+    this.identity = identity;
+}
+
+Color.prototype.events = function () {
     $('.all').mouseover(function(){
         $('#' + this.id).css("border", "3px solid white");
     });
@@ -32,16 +49,20 @@ function hoverColor() {
     });
     $('.all').click(function(){
         var bckgrnd = $( this ).css( 'background-color' );
-        var colorInfo = '<p>' + $( this ).css( 'background-color' ) + '</p>';
-        $('#tiny').css( 'background-color', bckgrnd );
-        print2(colorInfo);
+        var colorInfo = bckgrnd + '<br />';
+        //$('#tinySquares').css( 'background-color', bckgrnd );
+        var colorBlock = '<div class="block" id="bk_' + this.id + '" style="background-color:' + bckgrnd + '"></div>';
+        selectedColorBlocks.push(colorBlock);
+        print3(selectedColorBlocks.join(""));
+        selectedColors.push(colorInfo);
+        print2(selectedColors.join(""));
+        console.log(selectedColorBlocks);
     });
 }
 
-for ( var i = 0; i < 100; i += 1) {
-    rgbColor = randomColor();
-    html += '<div class="all" id="color' + i + '" style="background-color:' + rgbColor + '"></div>';
-}
+var shell = new Color();
 
 print(html);
-hoverColor();
+shell.events();
+
+
